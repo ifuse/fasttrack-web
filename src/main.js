5603a07c2a7111243cdc38cf5b840bbbb026282a
Chamnan Longlum <chamnan@irainnovation.com>
2019-08-03 01:18:31 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from "vue"
+import "./plugins/vuetify"
+import App from "./App.vue"
+import router from "./router/"
+import store from "./store"
+import "./registerServiceWorker"
+import "roboto-fontface/css/roboto/roboto-fontface.css"
+import "font-awesome/css/font-awesome.css"
+import * as firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app")
+var config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID
+}
+firebase.initializeApp(config)
